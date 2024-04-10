2cf94cfc2069c748edd76e6a9442ba01e8519806
glopesdev <glopesdev@users.noreply.github.com>
2024-04-10 16:49:14 +0100

    Remove XML documentation from internal types

--- Interface/Harp.SoundCard/SampleType.cs
+++ Interface/Harp.SoundCard/SampleType.cs
@@ -1,18 +1,8 @@
 ﻿namespace Harp.SoundCard
 {
-    /// <summary>
-    /// Specifies the available bit depths for sound waveform samples.
-    /// </summary>
     internal enum SampleType : int
     {
-        /// <summary>
-        /// Specifies signed 32-bit samples.
-        /// </summary>
         Int32 = 0,
-
-        /// <summary>
-        /// Specifies single-precision 32-bit floating-point samples.
-        /// </summary>
         Float32 = 1
     }
 }
